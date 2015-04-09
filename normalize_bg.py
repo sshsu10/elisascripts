9f5ce2671d3e0848e16f69eb912f2312b90d5a38
Tim D. Smith <git@tim-smith.us>
2015-04-08 22:42:24 -0700

    normalize_bg: subtract background

--- normalize_bg.py
+++ normalize_bg.py
@@ -32,22 +32,15 @@ def main():
         print 'Computing background image'
         sorted_ar = ar.copy()
         sorted_ar.sort(0)
-        bg = sorted_ar[int(0.05*n)]
+        bg = sorted_ar[int(round(0.01*n,0))]
         print 'Saving background image'
         tf.imsave('background.tif', bg)
         del sorted_ar
 
     print 'Performing background normalization'
-    ar = ar.astype(np.double)
+    ar = ar.astype(np.int16)
     for i in range(n):
-        ar[i] /= bg
-
-    print 'Converting to 16-bit TIFF'
-    max_normed = (4095.0 / bg.min()) - 1
-    ar -= 1
-    ar *= 65535
-    ar /= max_normed
-    ar = ar.round()
+        ar[i] -= bg
     ar[ar < 0] = 0
     ar = ar.astype(np.uint16)
 
