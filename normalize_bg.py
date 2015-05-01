d9af4f8cbe2ef7068f87512d714beae515c828b1
Tim D. Smith <git@tim-smith.us>
2015-05-01 11:58:01 -0700

    normalize_bg: add subtract option

--- normalize_bg.py
+++ normalize_bg.py
@@ -16,6 +16,8 @@ def main():
     parser = argparse.ArgumentParser(description=_description)
     parser.add_argument('--output', '-o', metavar='normalized.tif',
                         default='normalized.tif', help='Output filename')
+    parser.add_argument('--subtract', action='store_true',
+            help='Subtract background instead of dividing and truncating')
     parser.add_argument('infile')
     args = parser.parse_args()
 
@@ -25,6 +27,8 @@ def main():
     ar = tf.stack_pages(t.pages)
     n = ar.shape[0]
 
+    percentile = 0.01 if args.subtract else 0.05
+
     if os.path.exists('background.tif'):
         print 'Reading background image'
         bg = tf.imread('background.tif')
@@ -32,22 +36,27 @@ def main():
         print 'Computing background image'
         sorted_ar = ar.copy()
         sorted_ar.sort(0)
-        bg = sorted_ar[int(0.05*n)]
+        bg = sorted_ar[int(round(percentile*n, 0))]
         print 'Saving background image'
         tf.imsave('background.tif', bg)
         del sorted_ar
 
     print 'Performing background normalization'
-    ar = ar.astype(np.double)
-    for i in range(n):
-        ar[i] /= bg
+    if not args.subtract:
+        ar = ar.astype(np.double)
+        for i in range(n):
+            ar[i] /= bg
 
-    print 'Converting to 16-bit TIFF'
-    max_normed = (4095.0 / bg.min()) - 1
-    ar -= 1
-    ar *= 65535
-    ar /= max_normed
-    ar = ar.round()
+        print 'Converting to 16-bit TIFF'
+        max_normed = (4095.0 / bg.min()) - 1
+        ar -= 1
+        ar *= 65535
+        ar /= max_normed
+        ar = ar.round()
+    else:
+        ar = ar.astype(np.int16)
+        for i in range(n):
+            ar[i] -= bg
     ar[ar < 0] = 0
     ar = ar.astype(np.uint16)
 
